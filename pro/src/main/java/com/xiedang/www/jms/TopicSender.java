7ae9442bafd15d97d8ded185e5209adb2135dbba
xiedang <1406970797@qq.com>
2018-11-07 13:09:32 +0800

    增加activemq

---
+++ pro/src/main/java/com/xiedang/www/jms/TopicSender.java
@@ -0,0 +1,26 @@
+package com.xiedang.www.jms;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+
+/**
+ * <p></p>
+ *
+ * @author : 谢当
+ * @date : 2018/11/6 16:52
+ */
+@Component
+public class TopicSender {
+
+    @Autowired
+    private JmsTemplate jmsTemplate;
+
+    /**
+     * 发送信息
+     * @param message
+     */
+    public void send(String message){
+        jmsTemplate.send(session -> session.createTextMessage(message));
+    }
+}
