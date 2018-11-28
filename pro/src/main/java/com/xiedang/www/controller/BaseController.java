e5610a4d3c544647bcc93034db9de0bfadd669e9
xiedang <1406970797@qq.com>
2018-11-28 09:27:19 +0800

    增加redis

--- pro/src/main/java/com/xiedang/www/controller/BaseController.java
+++ pro/src/main/java/com/xiedang/www/controller/BaseController.java
@@ -1,9 +1,12 @@
 package com.xiedang.www.controller;
 
+import com.xiedang.www.utils.CommonResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * <p></p>
@@ -17,9 +20,20 @@ public class BaseController {
     private Logger log = LoggerFactory.getLogger(this.getClass());
 
     @ExceptionHandler(Exception.class)
+    @ResponseBody
     public Object exceptionHandle(Exception e) {
-        e.printStackTrace();
-        log.error("服务器异常，{}",e);
-        return null;
+        CommonResult<Integer> result=new CommonResult<>(CommonResult.FAILURE_CODE);
+        result.setMessage(e.getMessage());
+        log.error("服务器异常，错误{}",e);
+        return result;
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    @ResponseBody
+    public Object httpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException e) {
+        CommonResult<Integer> result=new CommonResult<>(CommonResult.FAILURE_CODE);
+        result.setMessage(e.getMessage());
+        log.error("服务器异常，错误{}",e);
+        return result;
     }
 }
