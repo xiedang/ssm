5a940f9acb58dd070667917cf5e9b7a7728a7110
1406970797 <1406970797@qq.com>
2018-11-29 20:00:20 +0800

    提交工具类

--- common/src/main/java/com/xiedang/www/utils/ObjectUtil.java
+++ common/src/main/java/com/xiedang/www/utils/ObjectUtil.java
@@ -1,15 +1,26 @@
 package com.xiedang.www.utils;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * java对象和map的相互转换
+ * 对象工具类
  */
 public class ObjectUtil {
 
+    /**
+     * map装换成指定对象
+     * @param map
+     * @param beanClass
+     * @return
+     * @throws Exception
+     */
     public static Object mapToObject(Map<String, Object> map, Class<?> beanClass) throws Exception {
         if (map == null) {
             return null;
@@ -18,7 +29,7 @@ public class ObjectUtil {
         Field[] fields = obj.getClass().getDeclaredFields();
         for (Field field : fields) {
             int mod = field.getModifiers();
-            if(Modifier.isStatic(mod) || Modifier.isFinal(mod)){
+            if (Modifier.isStatic(mod) || Modifier.isFinal(mod)) {
                 continue;
             }
             field.setAccessible(true);
@@ -27,8 +38,14 @@ public class ObjectUtil {
         return obj;
     }
 
+    /**
+     * 对象转换成map
+     * @param obj
+     * @return
+     * @throws Exception
+     */
     public static Map<String, Object> objectToMap(Object obj) throws Exception {
-        if(obj == null){
+        if (obj == null) {
             return null;
         }
         Map<String, Object> map = new HashMap<>(16);
@@ -38,5 +55,29 @@ public class ObjectUtil {
             map.put(field.getName(), field.get(obj));
         }
         return map;
+    }
+
+    /**
+     * 根据方法名和参数获取指定对象的方法名的值
+     * @param methodName 方法名
+     * @param obj 指定对象
+     * @param objects 方法参数
+     * @return 返回对象
+     */
+    public static Object getMethodValue(String methodName, Object obj, Object... objects) throws Exception{
+        Object methodValue=null;
+        if(null==obj || StringUtils.isBlank(methodName)){
+            return null;
+        }
+        Method[] methods = obj.getClass().getDeclaredMethods();
+        if(null==methods){
+            return null;
+        }
+        for (Method m:methods) {
+           if(m.getName().equalsIgnoreCase(methodName)){
+               methodValue=m.invoke(obj,objects);
+           }
+        }
+        return methodValue;
     }
 }
