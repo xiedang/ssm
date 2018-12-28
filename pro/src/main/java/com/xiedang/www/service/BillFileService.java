174d7e40f475bdddc1f1fdc3dbd4072c2e3ed2ee
xiedang <1406970797@qq.com>
2018-12-28 09:34:08 +0800

    增加工具类

--- pro/src/main/java/com/xiedang/www/service/BillFileService.java
+++ pro/src/main/java/com/xiedang/www/service/BillFileService.java
@@ -14,5 +14,12 @@ import java.io.IOException;
  * @date : 2018/12/27 16:35
  */
 public interface BillFileService {
+
+    /**
+     * 文件上传
+     * @param filesVo 上传参数
+     * @param file 上传文件
+     * @return 附件实体类集合
+     */
     CommonResult<BillFiles> uploadFile(FilesVo filesVo, MultipartFile file) throws IllegalStateException, IOException;
 }
