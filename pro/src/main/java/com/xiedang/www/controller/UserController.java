1d3ee2e5e0db71e079e96bb56e2152864d156395
1406970797 <1406970797@qq.com>
2019-01-05 19:19:18 +0800

    bug修复

--- pro/src/main/java/com/xiedang/www/controller/UserController.java
+++ pro/src/main/java/com/xiedang/www/controller/UserController.java
@@ -21,10 +21,7 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * <p></p>
