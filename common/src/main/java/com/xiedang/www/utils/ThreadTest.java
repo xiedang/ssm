f8ae25a4160145a362cf00a7d3a4795bd2475ffb
xiedang <1406970797@qq.com>
2019-01-10 13:06:53 +0800

    增加工具类

--- common/src/main/java/com/xiedang/www/utils/ThreadTest.java
+++ common/src/main/java/com/xiedang/www/utils/ThreadTest.java
@@ -1,8 +1,9 @@
 package com.xiedang.www.utils;
 
+import com.alibaba.fastjson.JSONArray;
 import com.xiedang.www.utils.date.DateUtil;
 
-import java.util.Date;
+import java.util.*;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -16,7 +17,7 @@ import java.util.concurrent.Executors;
 public class ThreadTest {
 
     public static void main(String[] args) {
-        ExecutorService executorService= Executors.newCachedThreadPool();
+        ExecutorService executorService = Executors.newCachedThreadPool();
         CountDownLatch start = new CountDownLatch(1);
         CountDownLatch end = new CountDownLatch(5);
         try {
@@ -25,16 +26,16 @@ public class ThreadTest {
                 executorService.execute(() -> {
                     try {
                         start.await();
-                        System.out.println("当前为"+ j +"在"+ DateUtil.format(new Date(),"yyyy-MM-dd HH:mm:ss:S")+"结束运行");
+                        System.out.println("当前为" + j + "在" + DateUtil.format(new Date(), "yyyy-MM-dd HH:mm:ss:S") + "结束运行");
                     } catch (InterruptedException e) {
                         e.printStackTrace();
-                    }finally {
+                    } finally {
                         end.countDown();
                     }
                 });
             }
             executorService.execute(() -> {
-                System.out.println("在"+ DateUtil.format(new Date(),"yyyy-MM-dd HH:mm:ss:S")+"开始运行");
+                System.out.println("在" + DateUtil.format(new Date(), "yyyy-MM-dd HH:mm:ss:S") + "开始运行");
                 start.countDown();
             });
             end.await();
@@ -43,5 +44,23 @@ public class ThreadTest {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        test();
+    }
+
+    public static void test() {
+        List<Map<String, Object>> mapList = new ArrayList<>();
+        Map<String, Object> map = new HashMap<>(3);
+        Map<String, Object> map1 = new HashMap<>(3);
+        Map<String, Object> map2 = new HashMap<>(3);
+        map2.put("text", "3");
+        map2.put("url", "3");
+        map1.put("text", "2");
+        map1.put("url", "2");
+        map1.put("children", map2);
+        map.put("text", "1");
+        map.put("url", "1");
+        map.put("children", map1);
+        mapList.add(map);
+        System.out.println(JSONArray.toJSON(mapList));
     }
 }
