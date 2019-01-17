f6190579ad5b5618416bbaa2633ee6509f29ca9a
xiedang <1406970797@qq.com>
2019-01-17 14:05:57 +0800

    bug修复

--- pro/src/main/java/com/xiedang/www/task/BackupTask.java
+++ pro/src/main/java/com/xiedang/www/task/BackupTask.java
@@ -16,6 +16,9 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/**
+ * @author Administrator
+ */
 @Component
 public class BackupTask {
     private Logger log = LoggerFactory.getLogger(this.getClass());
